8d2ca7e967075650de73290879cc2b83ab1c31f9
Alastair Montgomery <alastair.montgomery1@nhs.net>
2023-01-24 15:42:41 +0000

    Text output;

--- run_checks.sh
+++ run_checks.sh
@@ -2,6 +2,7 @@
 cd "$TEST_DIR" || exit
 pwd
 ls -l
-tfsec --config-file /home/security/config/.tfsec.yml --no-colour --format html --out /home/security/reports/tfsec_report.html
+tfsec --config-file /home/security/config/.tfsec.yml --no-colour --format text --out /home/security/reports/tfsec_report.txt
 tflint --config /home/security/config/.tflint.hcl ./ > /home/security/reports/tflint_report.txt
+cat /home/security/reports/*
 
