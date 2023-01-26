b3099c6609d76f9aff2dc177bc71ce551edeec5e
Alastair Montgomery <alastair.montgomery1@nhs.net>
2023-01-26 16:51:49 +0000

    Script tweaks

--- run_checks.sh
+++ run_checks.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cd "$TEST_DIR" || exit
-pwd
-ls -l
 tfsec --config-file /home/security/config/.tfsec.yml --no-colour --format text --out /home/security/reports/tfsec_report.txt
+sec_exit=$?
 tflint --config /home/security/config/.tflint.hcl ./ > /home/security/reports/tflint_report.txt
-cat /home/security/reports/*
-
+lint_exit=$?
+! (( $sec_exit || $lint_exit ))
