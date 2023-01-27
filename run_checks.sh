6cd511ab063c75087ef3b18e3cb0407ede905f8b
Alastair Montgomery <alastair.montgomery1@nhs.net>
2023-01-27 13:34:50 +0000

    Including tfvars file in the checks

--- run_checks.sh
+++ run_checks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$TEST_DIR" || exit
-tfsec --config-file /home/security/config/.tfsec.yml --no-colour --format text --out /home/security/reports/tfsec_report.txt
+tfsec --config-file /home/security/config/.tfsec.yml --no-colour --format text --out /home/security/reports/tfsec_report.txt --var-file=terraform.tfvars
 sec_exit=$?
-tflint --config /home/security/config/.tflint.hcl ./ > /home/security/reports/tflint_report.txt
+tflint --config /home/security/config/.tflint.hcl --var-file=terraform.tfvars ./ > /home/security/reports/tflint_report.txt
 lint_exit=$?
 ! (( $sec_exit || $lint_exit ))
