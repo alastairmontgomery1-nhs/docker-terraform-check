51e45a1a449221e4ffc59e4a2ef59a746da39571
Alastair Montgomery <alastair.montgomery1@nhs.net>
2023-02-13 14:47:12 +0000

    Ssplit tfsec, tflint

--- run_checks.sh
+++ run_checks.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 cd "$TEST_DIR" || exit
-tfsec --config-file /home/security/config/.tfsec.yml --no-colour --format text --out /home/security/reports/tfsec_report.txt --var-file=terraform.tfvars
-sec_exit=$?
-tflint --config /home/security/config/.tflint.hcl --var-file=terraform.tfvars ./ > /home/security/reports/tflint_report.txt
-lint_exit=$?
+sec_exit=0
+lint_exit=0
+if [ $TFSEC == true ]; then
+  tfsec --config-file /home/security/config/.tfsec.yml --no-colour --format text --out /home/security/reports/tfsec_report.txt --var-file=terraform.tfvars
+  sec_exit=$?
+fi
+if [ $TFLINT == true ]; then
+  tflint --config /home/security/config/.tflint.hcl --var-file=terraform.tfvars ./ > /home/security/reports/tflint_report.txt
+  lint_exit=$?
+fi
 ! (( $sec_exit || $lint_exit ))
